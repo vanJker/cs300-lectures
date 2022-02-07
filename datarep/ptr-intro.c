2daf46342f0f13c6e60f18422e1b783f564044f5
Malte Schwarzkopf <malte@cs.brown.edu>
2022-02-07 13:33:57 -0500

    Fixes

--- datarep/ptr-intro.c
+++ datarep/ptr-intro.c
@@ -4,18 +4,17 @@ void f() {
     int local = 1;
 
     printf("value of local: %d\n", local);
-    printf("address of local: %p\n", pointer);
 
     int* pointer = &local;
-
-    printf("address of pointer: %p\n", pointerpointer);
+    printf("address of local: %p\n", pointer);
+    printf("address of pointer: %p\n", &pointer);
 
     int** pointerpointer = &pointer;
     printf("address of pointerpointer: %p\n", &pointerpointer);
 
     printf("value of local through pointerpointer: %d\n",
         **pointerpointer);
-    printf("address of local through pointerpointer: %d\n",
+    printf("address of local through pointerpointer: %p\n",
         *pointerpointer);
 }
 
